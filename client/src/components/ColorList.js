a1743cd9b30c2c8cbe39bb6ad3468046f2c536c3
AislynnEdmiston <davidandaislynn@gmail.com>
2020-01-25 13:29:06 -0500

    completed delete and save edit

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { AxiosWithAuth as axios } from "../utils/AxiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, refresh }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -18,13 +18,24 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    
+    axios().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data)
+        setEditing(false)
+      })
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axios().delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(refresh())
+    .then(res => {
+      console.log('Color was Deleted', res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   };
 
   return (
