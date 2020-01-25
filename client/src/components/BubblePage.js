6bfe058d4ae48ac56af6ed2d1a74e28ea09c0bd6
AislynnEdmiston <davidandaislynn@gmail.com>
2020-01-25 13:03:36 -0500

    created my useEffect to get the colors

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { AxiosWithAuth as axios } from "../utils/AxiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    axios().get('http:localhost:5000/api/colors')
+      .then(res => {
+        console.log(res.data)
+        if(localStorage.getItem('token')){
+          setColorList(
+            res.data
+          )
+        }
+      })
+      .catch(err => console.log(err))
+  }, [refresh])
+
+
 
   return (
     <>
