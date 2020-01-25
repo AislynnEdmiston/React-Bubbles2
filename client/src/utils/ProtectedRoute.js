ecd7623639e377bd17ef9a24903c8caaa937b6fb
AislynnEdmiston <davidandaislynn@gmail.com>
2020-01-25 13:39:58 -0500

    updated protected route

--- client/src/utils/ProtectedRoute.js
+++ client/src/utils/ProtectedRoute.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route, Redirect } from 'react-router-dom';
 
-export default function ProtectedRoute() {
+export default function ProtectedRoute(props) {
+    const {component: Component, ...rest} = props
+
     return (
         <Route { ...rest }
             render= {(renderProps) => {
